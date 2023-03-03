657e80fb1f1cda264d665409f9e5c0ac5fa02a6e
jotapemattos <jpmrc49@gmail.com>
2023-03-02 21:40:38 -0300

    fixing the header issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,8 +30,15 @@ const Header = ({isOpen, setIsOpen}: HeaderProps) => {
       </div>
       {!isOpen ? 
         (
-          <div className="text-slate-300">
-            AAAAAAAAAAA
+          <div>
+            <div className="w-1/3 p-4 lg:p-12 lg:hidden flex items-center justify-center">
+              <button 
+                className="w-full h-full"
+                onClick={() => setIsOpen(true)}
+              >
+                <List size={32} weight='fill' color="#03fcb1"/>
+              </button>
+            </div>
           </div>
         ):(
           <div>
