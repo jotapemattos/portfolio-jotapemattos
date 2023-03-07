9c97c85da3f58e383e9de3c69bdd1f52152dffff
jotapemattos <jpmrc49@gmail.com>
2023-03-07 16:41:58 -0300

    update the projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -14,13 +14,15 @@ const pomodoroDescription = 'Project developed for my portfolio. However, it was
 
 const movieDescription = 'MyMDB is a search application that simulates IMDB. You can find any type of movie or series, searching by name, genre, latest releases, etc. It was developed with the objective of deepening my knowledge in react and API consumption!'
 
+const comingSoonDescription = 'Come back soon, the next project will be waiting for you!'
+
 export const Projects = () => {
 
   return (
     <div className='projects min-w-full min-h-full overflow-hidden'>
       <Link to='/' className='flex pt-28 items-center justify-start gap-2 pl-4 md:pl-4 lg:pl-28 xl:pl-24'>
         <button className='p-2 rounded-lg flex items-center justify-center gap-2 text-slate-300 font-semibold text-xl hover:bg-white/5 transition-colors duration2'>
-          <ArrowCircleLeft size={28} color="#cbd5e1" weight="fill" />Back
+          <ArrowCircleLeft size={28} color="#cbd5e1" weight="fill"/>Back
         </button>
       </Link>
       <div className="px-6 lg:px-28 pt-6 grid grid-cols-1 xl:grid-cols-2 items-center justify-between lg:gap-6">
@@ -87,12 +89,12 @@ export const Projects = () => {
           transition={{ duration: 1.2 }}
           className='flex flex-col lg:flex-row'
         >
-          <h1 className='text-3xl font-main text-slate-300 font-bold lg:hidden'>04. </h1>
+          <h1 className='text-3xl font-main text-slate-300 font-bold lg:hidden'>04. Coming Soon</h1>
           <img src={comingsoon} alt="" className='lg:w-1/2 lg:h-full w-full'/>
           <div className='w-full h-96 py-4 flex flex-col items-start justify-start gap-16'>
-                <h1 className='text-3xl font-main text-slate-300 font-bold hidden lg:block'>04. </h1>
+                <h1 className='text-3xl font-main text-slate-300 font-bold hidden lg:block'>04. Coming Soon</h1>
             <div className='w-full lg:w-96 flex flex-col justify-between gap-8'>
-              <p className='w-full lg:w-96 text-justify text-lg text-slate-300'>{''}</p>
+              <p className='w-full lg:w-96 text-justify text-lg text-slate-300'>{comingSoonDescription}</p>
               <div className='flex w-full justify-around'>
                 <a href="#" target='_blank'><BsGithub size={24} color="#A5B4FC" className="hover:fill-contrast-color hover:-translate-y-1 hover:scale-110 transition-all duration-300"/></a>
                 <a href="#" target='_blank'><ArrowSquareOut size={26} color="#A5B4FC" weight="fill" className="hover:fill-contrast-color hover:-translate-y-1 hover:scale-110 transition-all duration-300"/></a>
