9b7d92224850cfec658314dff4e08a6800e142b1
jotapemattos <jpmrc49@gmail.com>
2023-03-10 12:08:03 -0300

    update the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import SocialMedia from './SocialMedia'
 
 const Footer = () => {
   return (
-    <div id='contact' className='footer min-h-footer max-[320px]:h-5/6 max-[375px]:h-full lg:h-1200px xl:h-3/4 w-full flex items-center justify-center'>
+    <div id='contact' className='footer min-h-footer max-[320px]:h-full max-[375px]:h-full lg:h-1200px xl:h-3/4 w-full flex items-center justify-center'>
       <div className='w-4/5 h-full flex flex-col items-center justify-between max-[320px]:pt-10 max-[375px]:pt-20 md:pt-10 lg:pt-96 xl:pt-44 text-slate-300 gap-20'>
         <div className='flex flex-col items-center justify-center gap-8 max-[375px]:pt-10'>
           <h1 className="text-4xl text-center font-poppins font-extrabold">Get in touch</h1>
