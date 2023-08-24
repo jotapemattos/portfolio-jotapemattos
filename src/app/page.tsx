85f9d24d617035abb5d8bc16022d2c85f42b6217
João Pedro <jpmrc49@gmail.com>
2023-08-24 08:41:58 -0300

    build the main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { MainContent } from '@/components/HomeContent/MainContent';
+import SocialMedia from '@/components/SocialMedia';
+
 export default function Home() {
   return (
-    <>
-      <div>oi</div>
-    </>
+    <div className="max-w-full min-h-full h-full overflow-hidden">
+      <MainContent />
+      <SocialMedia />
+    </div>
   );
 }
