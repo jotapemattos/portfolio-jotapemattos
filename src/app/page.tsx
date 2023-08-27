8ccd2af0ee3b9706ad95036056900009f237c9ef
João Pedro <jpmrc49@gmail.com>
2023-08-27 15:54:03 -0300

    create a about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import About from '@/components/HomeContent/About';
 import { MainContent } from '@/components/HomeContent/MainContent';
-import SocialMedia from '@/components/SocialMedia';
 
 export default function Home() {
   return (
-    <div className="max-w-full min-h-full h-full overflow-hidden">
+    <main className="max-w-full min-h-full overflow-hidden">
       <MainContent />
-      <SocialMedia />
-    </div>
+      <About />
+    </main>
   );
 }
