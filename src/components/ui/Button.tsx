f33965d9dedc25620d915837eecc61419d419b94
João Pedro <jpmrc49@gmail.com>
2023-08-27 16:33:04 -0300

    style: change the text color

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-background hover:bg-primary/90',
+        default: 'bg-primary text-background hover:opacity-80',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
@@ -21,7 +21,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        lg: 'h-11 rounded-md px-4',
         icon: 'h-10 w-10'
       }
     },
