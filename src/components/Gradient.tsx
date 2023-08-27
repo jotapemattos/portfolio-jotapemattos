c3980d36373997a79923810b13a4d546776b6a8e
João Pedro <jpmrc49@gmail.com>
2023-08-27 15:58:55 -0300

    style: create a gradient

---
+++ src/components/Gradient.tsx
@@ -0,0 +1,7 @@
+const Gradient = () => {
+  return (
+    <div className="absolute top-0 right-1/2 h-[188px] w-[350px] -translate-y-1/2 translate-x-1/2 rounded-3xl bg-secondary opacity-30 dark:opacity-20 blur-3xl" />
+  );
+};
+
+export default Gradient;
