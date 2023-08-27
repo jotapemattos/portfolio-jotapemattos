7b7691007956b7d2699fdd08d1ad810e8d9469d3
João Pedro <jpmrc49@gmail.com>
2023-08-27 16:08:21 -0300

    create a noise texture for the background

---
+++ src/components/Texture.tsx
@@ -0,0 +1,7 @@
+const Texture = () => {
+  return (
+    <div className="mix bg-[url(../assets/noise.svg)] pointer-events-none fixed z-50 h-full w-full opacity-70 mix-blend-soft-light"></div>
+  );
+};
+
+export default Texture;
