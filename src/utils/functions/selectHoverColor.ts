29f1b47f0ccdc161ae00bd516ce287d22808903e
João Pedro <jpmrc49@gmail.com>
2023-09-07 14:35:19 -0300

    style: add transition to hover effect

--- src/utils/functions/selectHoverColor.ts
+++ src/utils/functions/selectHoverColor.ts
@@ -9,14 +9,14 @@ export type Tech =
   | 'git';
 
 const possibleColorsClassnames: Record<Tech, string> = {
-  html: 'group-hover:bg-[#E44F26]',
-  css: 'group-hover:bg-[#33A9DC]',
-  javascript: 'group-hover:bg-[#FFCA28]',
-  typescript: 'group-hover:bg-[#007ACC]',
-  'react.js': 'group-hover:bg-[#61DAFB]',
-  'next.js': 'group-hover:bg-[#9B9B9B]',
-  'tailwind css': 'group-hover:bg-[#56BEB3]',
-  git: 'group-hover:bg-[#F05033]'
+  html: 'group-hover:bg-[#E44F26] transistion-all duration-300',
+  css: 'group-hover:bg-[#33A9DC] transistion-all duration-300',
+  javascript: 'group-hover:bg-[#FFCA28] transistion-all duration-300',
+  typescript: 'group-hover:bg-[#007ACC] transistion-all duration-300',
+  'react.js': 'group-hover:bg-[#61DAFB] transistion-all duration-300',
+  'next.js': 'group-hover:bg-[#9B9B9B] transistion-all duration-300',
+  'tailwind css': 'group-hover:bg-[#56BEB3] transistion-all duration-300',
+  git: 'group-hover:bg-[#F05033] transistion-all duration-300'
 };
 
 export const getHoverColor = (name: Tech) => {
