bdc3a7e961c89346fbc4eb8ee00db247442ba0c8
João Pedro <jpmrc49@gmail.com>
2023-09-07 13:02:25 -0300

    change the text about me into a more detailed text

--- src/components/HomeContent/About.tsx
+++ src/components/HomeContent/About.tsx
@@ -1,42 +1,76 @@
+'use client';
+
+import Image from 'next/image';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '../ui/HoverCard';
 import ToolsCards from './ToolsCards';
+import ifsp from '../../assets/ifsp.svg';
+import Gradient from '../Gradient';
 
 const About = () => {
   return (
-    <div className="w-full max-h-full flex items-center justify-center p-8">
-      <div className="w-[1300px] flex flex-col gap-20">
-        <section className="w-full h-fit flex flex-col items-start justify-center gap-4 bg-background rounded-xl">
-          <h1 id="about" className="font-bold text-primary text-3xl">
-            About me
+    <div
+      id="about"
+      className="about w-full max-h-full flex items-center justify-center p-8 bg-background overflow-hidden relative"
+    >
+      <div className="w-[1300px] flex flex-col gap-20 relative">
+        <section className="w-full h-fit flex flex-col items-start justify-center gap-10 text-justify">
+          <h1 className="font-bold text-primary text-3xl">
+            Introducing myself
           </h1>
-          <div className="flex flex-col h-auto items-start gap-2">
-            <p className="">
-              I&apos;m a frontend developer who is passionate about solving
-              problems and learning the best way to do it. I love building
-              charming interfaces and I&apos;m always very excited to discover
-              more and more about the languages, frameworks and libraries I use,
-              and also learn new technologies.
+          <article className="flex flex-col h-auto items-start gap-2">
+            <h2 className="font-bold text-primary text-2xl">How I started?</h2>
+            <p>
+              The genesis of my software engineering journey began with a
+              pivotal moment in early 2022 when a friend introduced me to the
+              profound capabilities of a software engineer. At the time, I
+              remember he made a simple clone of Twitter, the home page. This
+              further fueled my long-standing fascination with technology, which
+              had been nurtured since my childhood. It is this enduring passion
+              that propelled me to my current position.
+            </p>
+          </article>
+
+          <article className="flex flex-col h-auto items-start gap-2 ">
+            <h2 className="font-bold text-primary text-2xl">Nowadays</h2>
+            <p className="text-justify">
+              Now I am a dedicated frontend developer, driven by a relentless
+              desire to solve complex problems and continually enhance my
+              knowledge. I take great pride in crafting aesthetically pleasing
+              and user-friendly interfaces. My enthusiasm for my work is matched
+              only by my fervor for exploring and mastering the languages,
+              frameworks, and libraries that underpin modern web development.
+              Additionally, I am always eager to embrace and learn from emerging
+              technologies.
             </p>
             <p className="flex items-center gap-1">
               {' '}
-              I am currently graduating in Systems Analysis and Development at
-              <a
-                href="https://www.ifsp.edu.br/"
-                target="_blank"
-                className="hover:text-emerald-500 hover:underline transition-colors duration-200"
-                rel="noreferrer"
-              >
-                @IFSP.
-              </a>
+              Presently, I am in the process of completing my education,
+              pursuing a degree in Systems Analysis and Development at
+              <HoverCard>
+                <HoverCardTrigger
+                  href="https://www.ifsp.edu.br/"
+                  target="_blank"
+                  className="hover:text-emerald-600 hover:cursor-pointer underline transition-all duration-200"
+                >
+                  @IFSP.
+                </HoverCardTrigger>
+                <HoverCardContent className="flex items-start justify-center gap-2 text-sm bg-background">
+                  <Image src={ifsp} alt="IFSP logo" width={40} />
+                  <p>IFSP - Instituto Federal de São Paulo</p>
+                </HoverCardContent>
+              </HoverCard>
             </p>
-          </div>
+            <br />
+          </article>
         </section>
-        <section className="w-full h-full flex flex-col items-start justify-center gap-4">
+        <section className="w-full h-full flex flex-col items-start justify-center gap-4 relative z-50">
           <h1 className="font-bold text-primary text-3xl">Stack</h1>
-          <div className="grid grid-cols-3 gap-6">
+          <div className="grid grid-cols-3 gap-6 ">
             <ToolsCards />
           </div>
         </section>
       </div>
+      <Gradient className="absolute bottom-0 right-1/2 translate-y-2/3 translate-x-1/2 bg-secondary h-[200px] w-[650px] rounded-full opacity-25 blur-3xl" />
     </div>
   );
 };
