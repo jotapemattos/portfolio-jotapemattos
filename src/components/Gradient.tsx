8aa777977abfc9e9d7b71c44c1b45199d1becf0d
João Pedro <jpmrc49@gmail.com>
2023-09-07 12:47:50 -0300

    refactor: make styles be passed as props

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -1,7 +1,7 @@
-const Gradient = () => {
-  return (
-    <div className="absolute top-0 right-1/2 h-[188px] w-[350px] -translate-y-1/2 translate-x-1/2 rounded-3xl bg-secondary opacity-30 dark:opacity-20 blur-3xl" />
-  );
+import { HTMLProps } from 'react';
+
+const Gradient: React.FC<HTMLProps<HTMLDivElement>> = ({ ...props }) => {
+  return <div {...props} />;
 };
 
 export default Gradient;
