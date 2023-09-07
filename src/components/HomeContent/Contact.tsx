a9479f72ae7312b106bf02024b2a550d147b7ad3
João Pedro <jpmrc49@gmail.com>
2023-09-07 13:03:30 -0300

    feat: build the contact session

---
+++ src/components/HomeContent/Contact.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Copy, Send } from 'lucide-react';
+import { useToast } from '../../hooks/use-toast';
+
+const Contact = () => {
+  const { toast } = useToast();
+
+  const showToast = () => {
+    toast({
+      title: 'Successfully copied!',
+      description: '"jpmrc49@gmail.com" copied to clipboard'
+    });
+  };
+
+  return (
+    <>
+      <div
+        id="contact"
+        className="contact w-full max-h-full flex flex-col items-center justify-center gap-16 pt-20 pb-10"
+      >
+        <span className="flex flex-col items-center justify-center gap-4">
+          <p>Do you have an idea you want to get off the ground?</p>
+          <h1 className="text-primary text-3xl font-bold">
+            Let&apos;s talk about it
+          </h1>
+        </span>
+        <span className="flex items-center gap-4 px-4 py-2 h-10 text-md border border-input rounded-md">
+          <p className="text-md">jpmrc49@gmail.com</p>
+          <CopyToClipboard text="jpmrc49@gmail.com" onCopy={() => showToast()}>
+            <Copy size={18} className="hover:cursor-pointer" />
+          </CopyToClipboard>
+          <a href="mailto:jpmrc49@gmail.com">
+            <Send size={18} />
+          </a>
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
