65e105c162c57ea087cc98ffb09939183525b120
João Pedro <jpmrc49@gmail.com>
2023-09-07 12:53:53 -0300

    refactor: turn the div into a self-closed element

--- src/components/Texture.tsx
+++ src/components/Texture.tsx
@@ -1,6 +1,6 @@
 const Texture = () => {
   return (
-    <div className="mix bg-[url(../assets/noise.svg)] pointer-events-none fixed z-50 h-full w-full opacity-70 mix-blend-soft-light"></div>
+    <div className="mix bg-[url(../assets/noise.svg)] pointer-events-none fixed z-50 h-full w-full opacity-70 mix-blend-soft-light" />
   );
 };
 
