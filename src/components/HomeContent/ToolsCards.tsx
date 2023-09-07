77e44500f6ebbef720d910674a892f9186bfcb04
João Pedro <jpmrc49@gmail.com>
2023-09-07 14:19:46 -0300

    style: implements grayscale effect on hover in the tech's logos

--- src/components/HomeContent/ToolsCards.tsx
+++ src/components/HomeContent/ToolsCards.tsx
@@ -9,20 +9,22 @@ const ToolsCards = () => {
       {tools.map((tool: ToolsProps, i) => (
         <div
           key={i}
-          className="bg-gradient-to-tl from-background to-primary/30 rounded-lg p-[0.5px] group"
+          className="w-56 h-16 group rounded-lg bg-background backdrop-blur-md flex items-center gap-4 p-4 overflow-hidden border-t border-r border-primary/10 drop-shadow-[0_0_4px_rgba(49,49,49,0.2)]"
         >
-          <div className="w-56 h-16 rounded-lg bg-zinc-950/90 backdrop-blur-md flex items-start gap-4 p-4 overflow-hidden hover:cursor-pointer">
-            <span
-              className={cn(
-                'fixed top-0 right-3 h-4 w-12 rounded-3xl bg-transparent opacity-90 blur-2xl',
-                getHoverColor(tool.name.toLowerCase() as Tech)
-              )}
-            ></span>
-            <span className="w-10 h-10 rounded-md">
-              <Image src={tool.image} alt={tool.name + 'logo'} />
-            </span>
-            <p className="text-sm">{tool.name}</p>
-          </div>
+          <span
+            className={cn(
+              'fixed top-0 right-3 h-4 w-12 rounded-3xl bg-transparent opacity-90 blur-2xl',
+              getHoverColor(tool.name.toLowerCase() as Tech)
+            )}
+          />
+          <span className="w-6 h-6 group">
+            <Image
+              src={tool.image}
+              alt={tool.name + 'logo'}
+              className="grayscale group-hover:grayscale-0"
+            />
+          </span>
+          <p className="text-xs">{tool.name}</p>
         </div>
       ))}
     </>
