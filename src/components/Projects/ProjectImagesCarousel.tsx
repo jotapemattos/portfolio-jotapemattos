4700774cd9d5247e20f30de3f955ac3c49ce09b6
João Pedro <jpmrc49@gmail.com>
2023-09-22 14:32:41 -0300

    feat: create a carousel component for project images

---
+++ src/components/Projects/ProjectImagesCarousel.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ProjectsProps } from '@/data/projects';
+
+interface singleProjectProps {
+  singleProject: ProjectsProps;
+}
+
+export const ProjectImagesCarousel = (singleProject: singleProjectProps) => {
+  return <div className="w-full h-full"></div>;
+};
