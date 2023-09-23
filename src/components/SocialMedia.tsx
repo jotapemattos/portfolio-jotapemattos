9570bb7f32e21176ffe2c7c02ffe5a827c287861
João Pedro <jpmrc49@gmail.com>
2023-09-23 13:35:13 -0300

    feat: replaces email link with twitter link

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -5,7 +5,7 @@ import { getIconColors } from '@/utils/functions/pickColor';
 import { motion as m } from 'framer-motion';
 import { useTheme } from 'next-themes';
 
-import { GithubIcon, LinkedinIcon, Mail } from 'lucide-react';
+import { GithubIcon, LinkedinIcon, Twitter } from 'lucide-react';
 
 type Theme = 'dark' | 'light';
 
@@ -62,15 +62,15 @@ const SocialMedia = () => {
         className="bg-gradient-to-tl from-background to-primary/30 rounded-full p-[0.3px] group"
       >
         <a
-          href="mailto:jpmrc49@gmail.com"
+          href="https://twitter.com/devjotape"
           target="_blank"
           className="rounded-full flex items-center justify-center p-[12px] bg-background group-hover:bg-background/90"
           rel="noreferrer"
         >
-          <Mail
+          <Twitter
             color={iconColor}
             size={18}
-            className="group-hover:stroke-yellow-600 dark:group-hover:stroke-yellow-400 transition-colors duration-300"
+            className="group-hover:stroke-blue-500 dark:group-hover:stroke-blue-400 transition-colors duration-300"
           />
         </a>
       </m.span>
