5ad86116550fcd0a08e40f69bf8d7d56007efe3b
João Pedro <jpmrc49@gmail.com>
2023-09-25 12:34:14 -0300

    feat: creates a carousel component for project images

--- src/components/Projects/ProjectImagesCarousel.tsx
+++ src/components/Projects/ProjectImagesCarousel.tsx
@@ -1,13 +1,48 @@
 'use client';
-
+import useEmblaCarousel from 'embla-carousel-react';
 import { ProjectsProps } from '@/data/projects';
+import Image from 'next/image';
+import { useCallback } from 'react';
+import { Button } from '../ui/Button';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 
 interface singleProjectProps {
   singleProject: ProjectsProps;
 }
 
-export const ProjectImagesCarousel = (singleProject: singleProjectProps) => {
+export const ProjectImagesCarousel = ({
+  singleProject
+}: singleProjectProps) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
   return (
-    <div className="w-full h-full">{singleProject.singleProject.name}</div>
+    <>
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="flex">
+          {singleProject.images.map((image, index) => (
+            <div className="embla__slide " key={index}>
+              <Image src={image} alt="images" className="" />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <span className="flex items-center justify-between w-full ">
+        <Button onClick={scrollPrev} variant="ghost">
+          <ArrowLeft />
+        </Button>
+        <Button onClick={scrollNext} variant="ghost">
+          <ArrowRight />
+        </Button>
+      </span>
+    </>
   );
 };
