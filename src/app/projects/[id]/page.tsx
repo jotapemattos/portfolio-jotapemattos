335826f1e43f92e0e4da2c5665e82a70aef3f11d
João Pedro <jpmrc49@gmail.com>
2023-09-26 13:55:27 -0300

    feat: add github and website links

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,7 +1,10 @@
 import ToolsCards from '@/components/HomeContent/ToolsCards';
 import { ProjectImagesCarousel } from '@/components/Projects/ProjectImagesCarousel';
+import { Button } from '@/components/ui/Button';
 
 import { ProjectsProps, projects } from '@/data/projects';
+import { ArrowUpRight, GithubIcon } from 'lucide-react';
+import Link from 'next/link';
 
 interface SlugProps {
   params: {
@@ -13,13 +16,35 @@ const page = ({ params }: SlugProps) => {
   const [singleProject] = projects.filter(
     (project: ProjectsProps) => project.id === Number(params.id)
   );
-  console.log('oi');
+
   return (
     <main className="w-full min-h-full flex items-center justify-center pt-40 pb-20">
       <div className="w-1/2 h-full flex flex-col items-start gap-6">
-        <h1 className="text-3xl font-bold">
-          0{singleProject.id}.{singleProject.name}
-        </h1>
+        <header className="w-full flex items-center justify-between">
+          <h1 className="text-3xl font-bold">
+            0{singleProject.id}. {singleProject.name}
+          </h1>
+          <span className="flex items-center gap-6">
+            <Link href={singleProject.githubRepo} target="_blank">
+              <Button
+                variant="outline"
+                className="flex items-center gap-2 hover:text-primary/75 transition-colors duration-300"
+              >
+                Github
+                <GithubIcon size={18} />
+              </Button>
+            </Link>
+            <Link href={singleProject.website} target="_blank">
+              <Button
+                variant="outline"
+                className="flex items-center gap-2 hover:text-tertiary transition-colors duration-300"
+              >
+                Deployed project
+                <ArrowUpRight size={18} />
+              </Button>
+            </Link>
+          </span>
+        </header>
         <ProjectImagesCarousel singleProject={singleProject} />
         <article className="space-y-2 text-justify">
           <h2 className="text-2xl font-bold italic">Elucidation</h2>
