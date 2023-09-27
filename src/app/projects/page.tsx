b837dd05aa646744f8ce75191ad47dcc29f069ee
João Pedro <jpmrc49@gmail.com>
2023-09-27 12:20:29 -0300

    style: add responsiveness

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,10 +3,10 @@ import { ProjectCards } from '@/components/Projects/ProjectCards';
 const page = () => {
   return (
     <main className="w-full min-h-full flex items-center justify-center pt-40 pb-20">
-      <div className="w-1/2 h-full flex flex-col items-start gap-20">
+      <div className="w-full md:w-1/2 h-full flex flex-col items-start gap-20 p-4 sm:p-0">
         <section className="space-y-4">
           <h1 className="font-bold text-primary text-3xl">Projects</h1>
-          <p className="text-primary">
+          <p className="text-primary text-justify">
             Here you can find some of what I&apos;m capable to do. These are
             projects that I put a lot of effort into, and through the code, they
             bring solutions to problems faced by people.
