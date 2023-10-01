d2742f3e922a51584bad6595825a171c0a01ea33
João Pedro <jpmrc49@gmail.com>
2023-10-01 20:38:52 -0300

    delete useless component

--- src/components/Texture.tsx
+++
@@ -1,7 +0,0 @@
-const Texture = () => {
-  return (
-    <div className="mix bg-[url(../assets/noise.svg)] pointer-events-none fixed z-50 h-full w-full opacity-70 mix-blend-soft-light" />
-  );
-};
-
-export default Texture;
