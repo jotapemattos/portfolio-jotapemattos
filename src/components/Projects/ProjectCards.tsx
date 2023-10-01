46a0c132bbf89dd653bfe4c9f3ff048102bdd0f6
João Pedro <jpmrc49@gmail.com>
2023-10-01 17:45:51 -0300

    style: add responsiveness

--- src/components/Projects/ProjectCards.tsx
+++ src/components/Projects/ProjectCards.tsx
@@ -8,13 +8,13 @@ export const ProjectCards = () => {
       {projects.map((project: ProjectsProps, i) => (
         <Link
           key={project.id}
-          className="w-full h-52 rounded-lg bg-zinc-100 dark:bg-zinc-900 backdrop-blur-md overflow-hidden grid grid-cols-2 gap-2 items-start border-t border-r border-primary/10 drop-shadow-[0_0_4px_rgba(49,49,49,0.2)] hover:scale-105 transition-transform duration-300"
+          className="w-full h-60 md:h-52 rounded-lg bg-zinc-100 dark:bg-zinc-900 backdrop-blur-md overflow-hidden grid grid-rows-2 md:grid-cols-2 gap-2 items-end border-t border-r border-primary/10 drop-shadow-[0_0_4px_rgba(49,49,49,0.2)] hover:scale-105 transition-transform duration-300"
           href={`/projects/${project.id}`}
         >
           <section className="w-full h-full">
             <Image src={project.thumbnail} alt={project.name + 'image'} />
           </section>
-          <section className="w-full h-full flex flex-col justify-between items-end p-4 text-primary">
+          <section className="w-full h-fit md:h-full flex flex-col justify-between items-end p-4 text-primary bg-zinc-100 dark:bg-zinc-900">
             <h1 className="text-xl font-bold">
               {i + 1}. {project.name}
             </h1>
