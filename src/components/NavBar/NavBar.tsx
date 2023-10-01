3d3dfc34ac206ee87feb003d12f000d01acd8e12
João Pedro <jpmrc49@gmail.com>
2023-10-01 17:42:51 -0300

    style: add responsivenes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -12,18 +12,18 @@ const NavBar = () => {
   };
 
   return (
-    <header className="w-full h-24 flex items-center justify-between p-2 md:p-10 fixed z-10 inset-0 backdrop-blur-sm">
-      <Link href="/" className="w-1/3" onClick={handleClick}>
+    <header className="w-full h-24 flex items-center justify-between gap-4 md:gap-0 p-2 md:p-10 fixed z-10 inset-0 backdrop-blur-sm">
+      <Link href="/" className="lg:w-1/3 hidden md:block" onClick={handleClick}>
         <img src="/logo-withoutbg.svg" alt="logo-image" className="w-8 h-8" />
       </Link>
-      <nav className="w-full md:w-4/5 lg:w-fit xl:w-fit h-14 flex items-center justify-center p-6 text-primary bg-background/30 bg-opacity-10 backdrop-blur-sm rounded-full drop-shadow-lg border border-border">
-        <Link onClick={() => window.scroll(0, 0)} href={'/'}>
+      <nav className="w-full md:w-fit h-14 flex items-center justify-center p-2 md:p-6 text-primary bg-background/30 bg-opacity-10 backdrop-blur-sm rounded-full drop-shadow-lg border border-border">
+        <Link onClick={handleClick} href={'/'}>
           <Button
             variant="ghost"
             className="flex items-center justify-center gap-2 font-medium"
             onClick={handleClick}
           >
-            <Home size={16} />
+            <Home size={16} className="max-[320px]:hidden min-[375px]:block" />
             Home
           </Button>
         </Link>
@@ -32,7 +32,10 @@ const NavBar = () => {
             variant="ghost"
             className="flex items-center justify-center gap-2 font-medium"
           >
-            <FolderOpenDot size={16} />
+            <FolderOpenDot
+              size={16}
+              className="max-[320px]:hidden min-[375px]:block"
+            />
             Projects
           </Button>
         </Link>
@@ -41,13 +44,13 @@ const NavBar = () => {
             variant="ghost"
             className="flex items-center justify-center gap-2 font-medium"
           >
-            <User2 size={16} />
+            <User2 size={16} className="max-[320px]:hidden min-[375px]:block" />
             Contact
           </Button>
         </Link>
         <ThemeToggle />
       </nav>
-      <section className=" w-1/3 hidden md:block">
+      <section className="lg:w-1/3 hidden md:block">
         <SocialMedia />
       </section>
     </header>
